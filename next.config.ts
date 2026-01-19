f942c3e7f8fdbeb93fff671057bafab95b86ce01
Rashad Mirza <rashad.mirza.404@gmail.com>
2026-01-19 11:29:55 -0600

    admin fixes

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: '**.kredit.az',
+        hostname: '**.kimlik.az',
         pathname: '/**',
       },
     ],
